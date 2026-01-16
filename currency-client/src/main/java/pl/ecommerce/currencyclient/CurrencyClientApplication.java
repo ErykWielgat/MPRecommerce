b564d896d7bcfa8cf40f9a6f7dd244b7355d0faa
ErykWielgat <eryk-wielgat@wp.pl>
2026-01-16 15:23:02 +0100

    poprawiono modul currency

--- currency-client/src/main/java/pl/ecommerce/currencyclient/CurrencyClientApplication.java
+++ currency-client/src/main/java/pl/ecommerce/currencyclient/CurrencyClientApplication.java
@@ -3,11 +3,12 @@ package pl.ecommerce.currencyclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// Używamy "excludeName" i podajemy pełną nazwę w cudzysłowie.
+// Dzięki temu nie musimy robić importu, który Ci nie działa.
+@SpringBootApplication(excludeName = "org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration")
 public class CurrencyClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CurrencyClientApplication.class, args);
     }
-
 }
