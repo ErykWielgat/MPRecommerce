b55ca60f62039c72fe911b3ccfc8d724233cf771
ErykWielgat <eryk-wielgat@wp.pl>
2026-01-16 14:22:42 +0100

    dodano modul waluty

---
+++ currency-client/src/main/java/pl/example/currencyclient/CurrencyClientApplication.java
@@ -0,0 +1,13 @@
+package pl.example.currencyclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyClientApplication.class, args);
+    }
+
+}
---
+++ currency-client/src/test/java/pl/example/currencyclient/CurrencyClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.example.currencyclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
